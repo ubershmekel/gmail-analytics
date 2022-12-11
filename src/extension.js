1e38816e7db0ca215f825fbee67908b8dd29b304
Yuval Greenfield <ubershmekel@gmail.com>
2022-12-10 16:28:56 -0800

    Shows the top sender at the top of gmail With a link to search for them

--- src/extension.js
+++ src/extension.js
@@ -18,6 +18,7 @@ function startExtension(gmail) {
     gmail.observe.on("load", () => {
         const userEmail = gmail.get.user_email();
         console.log("Hello, " + userEmail + ". This is your extension talking!");
+        setInterval(getTopMessages, 1000);
 
         gmail.observe.on("view_email", (domEmail) => {
             console.log("Looking at email:", domEmail);
@@ -29,4 +30,57 @@ function startExtension(gmail) {
             console.log("New compose window is opened!", compose);
         });
     });
+}
+
+function sortCounter(counter) {
+    const sorted = Object.entries(counter).sort(([, a], [, b]) => b - a);
+    // const sortedObj = Object.fromEntries(sorted);
+    const noSingles = sorted.filter((item) => item[1] > 1);
+    // console.log("singles count", sorted.length - noSingles.length);
+    // console.log(noSingles);
+    return noSingles;
+}
+
+function insertAfter(newNode, existingNode) {
+    existingNode.parentNode.insertBefore(newNode, existingNode.nextSibling);
+}
+
+function getTopMessages() {
+    const counter = {};
+    gmail.dom.visible_messages().map((it) => {
+        const sender = it.from.email;
+        if (!counter[sender]) {
+            counter[sender] = 0;
+        }
+        counter[sender] = counter[sender] + 1;
+    });
+    const topSenderList = sortCounter(counter);
+    const topSenderEmail = topSenderList[0][0];
+    const topSenderCount = topSenderList[0][1];
+    // console.log(topSender, topSenderCount);
+
+    const elementId = "gmail-analytics-widget";
+    const existingEl = document.getElementById(elementId);
+    const newEl = document.createElement("span");
+    newEl.id = elementId;
+    newEl.appendChild(document.createTextNode(`${topSenderCount} emails from`));
+
+    const link = document.createElement("a");
+    link.href = "#search/from: " + topSenderEmail;
+    link.appendChild(document.createTextNode(topSenderEmail));
+    newEl.appendChild(link);
+
+    if (existingEl) {
+        // replace existing
+        existingEl.parentNode.replaceChild(newEl, existingEl);
+    } else {
+        // insert new
+        const searchBarEl = gmail.dom.search_bar();
+        if (!searchBarEl) {
+            console.log("search bar not found");
+            return;
+        }
+        insertAfter(newEl, gmail.dom.search_bar()[0].parentNode.parentNode);
+    }
+
 }
